613cd85ae7a70ef61ae98c8652d02f0992fc534f
Duy Nguyen <dnlove@gmail.com>
2012-04-21 18:42:32 -0700

    working version of google tv app

---
+++ party-tv-server/src/snippet/Snippet.java
@@ -0,0 +1,32 @@
+
+package snippet;
+
+public class Snippet {
+	/*
+	public static void main(String[] args) {
+		import android.graphics.Bitmap;
+		import android.graphics.BitmapFactory;
+		import android.net.http.AndroidHttpClient;
+		import android.os.AsyncTask;
+		
+		import org.apache.http.Header;
+		import org.apache.http.HttpEntity;
+		import org.apache.http.HttpResponse;
+		import org.apache.http.HttpStatus;
+		import org.apache.http.client.methods.HttpGet;
+		
+		import java.io.File;
+		import java.io.FileOutputStream;
+		import java.io.IOException;
+		import java.io.InputStream;
+		import java.net.MalformedURLException;
+		import java.net.URL;
+		
+	}
+*/
+	/*
+	public static void main(String[] args) {
+		~/apps/googletv-android-samples/Panoramio/src/com/google/android/panoramio
+	}*/
+}
+
