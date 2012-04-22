08efee9868e816cdfe77577e6ff394dad87f6889
Chuck Huie <vidscmd@netscape.net>
2012-04-21 19:37:55 -0700

    compilable version

--- party-tv-client/src/partytv/client/PartyMainActivity.java
+++ party-tv-client/src/partytv/client/PartyMainActivity.java
@@ -1,15 +1,16 @@
 package partytv.client;
 
+import android.R;
 import android.app.Activity;
 import android.os.Bundle;
-import com.google.api.client.googleapis.extensions.android2.auth.GoogleAccountManager;
+
 
 public class PartyMainActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        // setContentView(R.layout.main);
     }
     
     public void test() {
